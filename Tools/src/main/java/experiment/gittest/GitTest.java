2975c10e8784d3a2e02c26601586f1962c895b93
liukun <liu_kun@msn.com>
2014-04-22 11:57:44 +0800

    git第二次提交

--- Tools/src/main/java/experiment/gittest/GitTest.java
+++ Tools/src/main/java/experiment/gittest/GitTest.java
@@ -8,5 +8,6 @@ package experiment.gittest;
 public class GitTest{
     public static void main( String[] args ){
         System.out.println( "git 测试" );
+        System.out.println( "git 第二次提交以前" );
     }
 }
