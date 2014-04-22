3edb0f64ba9a910b0562bb6d63994d2e730e9b6c
liukun <liu_kun@msn.com>
2014-04-22 12:25:01 +0800

    第二次晕了 (cherry picked from commit f9bb2d1)

--- Tools/src/main/java/experiment/gittest/GitTest1.java
+++ Tools/src/main/java/experiment/gittest/GitTest1.java
@@ -9,6 +9,9 @@ public class GitTest1{
     public static void main( String[] args ){
         System.out.println();
         System.out.println( "第二次push" );
+        System.out.println( "已经晕了" );
+        System.out.println( "第二次已经晕了" );
+        System.out.println( "侧地晕了" );
 
     }
 }
