239641f5e35b675f82d5eca563e0f7619023d83c
liukun <liu_kun@msn.com>
2014-05-30 18:34:34 +0800

    格式化了一下

--- Tools/src/main/java/experiment/netty/protowithgame/server/handler/HandlerManager.java
+++ Tools/src/main/java/experiment/netty/protowithgame/server/handler/HandlerManager.java
@@ -1,5 +1,6 @@
 package experiment.netty.protowithgame.server.handler;
 
+import com.bbz.tool.common.StrUtil;
 import com.google.common.collect.Maps;
 
 import java.util.Map;
@@ -10,8 +11,8 @@ import static experiment.protocolgen.MsgProtocol.MSG;
  * user         LIUKUN
  * time         2014-5-29 11:37
  * <p/>
- * 通过protobuf中定义的MSG,来获取相应的handler，这里就必须有个规则：
- * MSG里面的消息定义，，然后加上Handler就是相应的类，举例如下
+ * 通过protobuf中定义的MSG,来获取相应的handler，这里就必须有个规则：<br/>
+ * MSG里面的消息定义，然后加上Handler就是相应的类，举例如下<br/>
  * MSG:Login====>LoginHandler.java
  * <p/>
  * MSG里面的首字母推荐大写，不过不是硬性规定
@@ -19,10 +20,12 @@ import static experiment.protocolgen.MsgProtocol.MSG;
 
 public enum HandlerManager{
     INSTANCE;
+
     /**
      * handler包的字符串，根据实际情况来调整，后面的"."不要丢了
      */
     public static final String HANDLER_PACKAGE = "experiment.netty.protowithgame.server.handler.";
+
     private Map<MSG, AbstractHandler> map = Maps.newHashMap();
 
     HandlerManager(){
@@ -30,6 +33,7 @@ public enum HandlerManager{
             map.put( msg, buildHandler( msg ) );
         }
 
+        System.out.println( "消息——句柄对应信息：" );
         System.out.println( map );
     }
 
@@ -40,12 +44,12 @@ public enum HandlerManager{
     /**
      * 通过MSG，利用反射生成相应的处理句柄的实例
      *
-     * @param msg 包标示符
+     * @param msg   通信包标示符，来自proto文件定义
      * @return 相应的的handler类实例
      */
     private AbstractHandler buildHandler( MSG msg ){
         AbstractHandler handler = null;
-        String className = String.format( "%s%sHandler", HANDLER_PACKAGE, firstCharacterToUpper( msg.toString() ) );
+        String className = String.format( "%s%sHandler", HANDLER_PACKAGE, StrUtil.firstCharacterToUpper( msg.toString() ) );
 
         try {
             handler = (AbstractHandler) Class.forName( className ).newInstance();
@@ -57,18 +61,5 @@ public enum HandlerManager{
 
     public AbstractHandler getHandler( MSG msg ){
         return map.get( msg );
-    }
-
-    /**
-     * 首字母转换为大写
-     *
-     * @param str 输入的字符串
-     * @return 首字母大写的字符串
-     */
-    private String firstCharacterToUpper( String str ){
-        return (new StringBuilder())
-                .append( Character.toUpperCase( str.charAt( 0 ) ) )
-                .append( str.substring( 1 ) )
-                .toString();
     }
 }
